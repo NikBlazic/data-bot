fc07830d13e1761d462682f1e75e6ec766fd143c
NikBlazic <nik.blazic07@gmail.com>
2024-12-27 16:50:35 +0100

    Connected supabase

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from api.perplexity import make_perplexity_request
+from supabase import create_client, Client
 
 load_dotenv()
 
 YOUR_API_KEY = os.getenv("OPENAI_API_KEY")
+
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
 
 if __name__ == "__main__":
     if not YOUR_API_KEY:
